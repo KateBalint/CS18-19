ed530843075aa4a410fd092cf9242717c1e0398b
KateBalint <30415848+KateBalint@users.noreply.github.com>
2019-05-20 13:28:00 -0400

    Add files via upload

---
+++ GitDemo.java
@@ -0,0 +1,10 @@
+public class GitDemo
+{
+    public static void main (String[] args)
+    {
+        Scanner scan = new Scanner();
+        int first = scan.new();
+        int second = scan.new();
+        System.out.println(first + second / 2);
+    }
+}
