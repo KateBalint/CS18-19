5006dc781184604620d223e2d8ba700e82e72dfb
Srinitya Allam <AllamS20@pwcs-edu.org>
2019-05-20 13:31:48 -0400

    File Updated

--- GitDemo.java
+++ GitDemo.java
@@ -1,10 +1,11 @@
+import java.util.* // import statement needed
 public class GitDemo
 {
     public static void main (String[] args)
     {
         Scanner scan = new Scanner();
-        int first = scan.new();
-        int second = scan.new();
-        System.out.println(first + second / 2);
+        int first = scan.nextInt(); // Change to NextInt()
+        int second = scan.nextInt(); // Change to NextInt()
+        System.out.println((first + second) / 2); // First + second needed parentheses
     }
 }
